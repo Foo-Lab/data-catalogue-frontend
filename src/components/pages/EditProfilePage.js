4082d012fd938609cd6f01375c09f0472b2e47c9
botakveggie <minhanchoo13@gmail.com>
2023-02-06 13:39:07 +0000

    profile edit shows error page

--- src/components/pages/EditProfilePage.js
+++ src/components/pages/EditProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 import { string, element, instanceOf, func, bool } from 'prop-types';
 import { useHistory, useParams } from 'react-router-dom';
 import { Form, Button, Checkbox } from 'antd';
@@ -8,6 +8,16 @@ import PageHeader from '../PageHeader';
 import { checkIsDate, formatDate } from '../../utilities';
 
 import './EditPage.scss'; // uses same scss as EditPage.js
+
+const dataReducer = (state, action) => {
+    if (action.type === "USER_DATA") {
+        return {ok: true, value: action.value}
+    }
+    if (action.type === "ERROR") {
+        return {ok: false, value: action.value}
+    }
+    return {ok: null, value: null}
+};
 
 const EditProfilePage = ({
     name,
@@ -20,27 +30,38 @@ const EditProfilePage = ({
     console.log("Different edit page for profile");
     const history = useHistory();
     const { id } = useParams();
-    const [data, setData] = useState(null);
+    const [errorState, setErrorState] = useState(null);
+    const [data, dataDispatch] = useReducer(dataReducer, {ok: null, value: 'Loading...'});
 
     useEffect(() => {
         const fetchData = async () => {
-            const { result } = await getData(id);
-            console.log(result);
-            Object.keys(result).forEach(key => {
-                const field = result[key];
-                if (checkIsDate(field)) {
-                    result[key] = formatDate(field, false);
-                }
-            });
-            setData(result);
+            try {
+                const { result } = await getData(id);
+                console.log(result);
+                Object.keys(result).forEach(key => {
+                    const field = result[key];
+                    if (checkIsDate(field)) {
+                        result[key] = formatDate(field, false);
+                    }
+                });
+                dataDispatch({ type: "USER_DATA", value: result});
+            } catch (error) {
+                console.error(error);
+                dataDispatch({ type: "ERROR", value: error});
+            }
         }
 
         fetchData();
     }, [id]);
 
     const onFinish = async (values) => {
-        await onEdit(id, values);
-        history.goBack();
+        try {
+            await onEdit(id, values);
+            history.goBack();
+        } catch (error) {
+            console.error(error);
+            setErrorState(error)
+        }
     }
 
     const onCancel = () => history.goBack();
@@ -51,7 +72,7 @@ const EditProfilePage = ({
             labelAlign='left'
             labelCol={{ span: 6, offset: 1 }}
             wrapperCol={{ span: 19 }}
-            initialValues={{ ...data, password: '' }}
+            initialValues={{ ...data.value, password: '' }}
             onFinish={onFinish}
             scrollToFirstError
         >
@@ -66,6 +87,11 @@ const EditProfilePage = ({
                     },
                     {
                         validator: async (rule, value) => {
+                            if (f.name === 'password') {
+                                if (errorState) {
+                                    return Promise.reject(new Error(errorState))
+                                }
+                            }
                             if (f.name === 'newPassword') {
                                 if (value.length < 8) {
                                     return Promise.reject(new Error('Password must be at least 8 characters long.'));
@@ -110,9 +136,9 @@ const EditProfilePage = ({
                 showBackButton={showBackButton}
             />
             <div className='page-content'>
-                {data
+                {data.ok
                     ? renderForm()
-                    : null
+                    : <p>{`${data.value}`}</p>
                 }
             </div>
         </div>
