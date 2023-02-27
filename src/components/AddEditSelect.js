0ce89567d99a4dfef452a7cef32517375ca4d79b
botakveggie <minhanchoo13@gmail.com>
2023-02-27 14:55:43 +0000

    stop logging to console

--- src/components/AddEditSelect.js
+++ src/components/AddEditSelect.js
@@ -8,23 +8,17 @@ const { Option } = Select;
 const AddEditSelect = ({ options, field }) => (
     <Select
         optionFilterProp="children"
-        filterOption={(input, option) => {
-            console.log('input',input);
-            console.log('option',option);
-            console.log('option?.name ?? ',option?.children ?? '');
-            return (option?.children ?? '').toLowerCase().includes(input)
-        }}
-        filterSort={(optionA, optionB) =>{
-            console.log('optionA',optionA);
-            console.log('optionB',optionB);
-
-            return (optionA?.children ?? '').toLowerCase().localeCompare((optionB?.children ?? '').toLowerCase())
-        }}
+        filterOption={(input, option) =>
+            (option?.children ?? '').toLowerCase().includes(input)
+        }
+        filterSort={(optionA, optionB) =>
+            (optionA?.children ?? '').toLowerCase().localeCompare((optionB?.children ?? '').toLowerCase())
+        }
         style={{ textAlign: 'left' }}
         placeholder={`Select ${field}`}
         showSearch
     >
-        {console.log('options:', options)}
+        {/* {console.log('options:', options)} */}
         {options.map(d => (
             <Option
                 key={d.id}
